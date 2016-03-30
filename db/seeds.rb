3d16079159514674ab072a0960ab659da7ecd52a
Anton <lascivolin@gmail.com>
2016-03-30 17:50:13 +0300

    1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+dfjksdhf  kjsdfh kjh kasdkj kajsd hkjasd kjashdf kjasdhfjkasdjk ajksd jsd jkasdh fjkasdh fjkasdjkhaskjdfh kajsdh jkasdf
+
+require 'oprn'
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+#cards_list = [
+#  ["dog", "bigdog"],
+#  ["cat", "bigcat"],
+#  ["bird", "bigbird"]
+#]
+
+#cards_list.each do |original_text, translated_text|
+#  Card.create(original_text: original_text, translated_text: translated_text, review_date: Date.today.next_day(3))
+#end
